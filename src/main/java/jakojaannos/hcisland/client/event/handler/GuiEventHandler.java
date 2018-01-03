abf285155ec2fb3d771f5b02c4d7d40fc17ea459
Kailari <ilari97@hotmail.com>
2018-01-03 17:16:43 +0200

    Fixed default world type overriding customized when exiting customize

--- src/main/java/jakojaannos/hcisland/client/event/handler/GuiEventHandler.java
+++ src/main/java/jakojaannos/hcisland/client/event/handler/GuiEventHandler.java
@@ -4,7 +4,9 @@ import jakojaannos.hcisland.HardcoreIsland;
 import jakojaannos.hcisland.config.HCIslandConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiCreateWorld;
+import net.minecraft.client.gui.GuiCustomizeWorldScreen;
 import net.minecraft.client.gui.GuiScreenWorking;
+import net.minecraft.client.gui.GuiWorldSelection;
 import net.minecraft.world.WorldType;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,7 +23,7 @@ public class GuiEventHandler {
     public static void onOpenGui(GuiOpenEvent event) {
         // If opening Create World gui and not recreating, set default worldtype
         // HACK: If opening via "Recreate World" -option, previous gui is GuiScreenWorking. Minecraft.currentScreen changes AFTER the event so we can use that to identify a re-create
-        if (event.getGui() instanceof GuiCreateWorld && !(Minecraft.getMinecraft().currentScreen instanceof GuiScreenWorking)) {
+        if (event.getGui() instanceof GuiCreateWorld && Minecraft.getMinecraft().currentScreen instanceof GuiWorldSelection) {
             if (HCIslandConfig.client.makeWorldTypeDefault) {
                 int index = -1;
                 for (int i = 0; i < WorldType.WORLD_TYPES.length; i++) {
