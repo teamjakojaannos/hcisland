f634c9ce046f8f56c39b116be5e903eefa99fea8
toilari <ilari97@hotmail.com>
2019-06-03 11:26:14 +0300

    Simplified biome registration, no need to reinvent the wheel

--- src/main/java/jakojaannos/hcisland/HardcoreIsland.java
+++ src/main/java/jakojaannos/hcisland/HardcoreIsland.java
@@ -1,15 +1,13 @@
 package jakojaannos.hcisland;
 
-import jakojaannos.hcisland.init.HCIslandBiomes;
 import jakojaannos.hcisland.init.HCIslandLootTables;
 import jakojaannos.hcisland.world.WorldTypeHCIsland;
 import net.minecraft.world.WorldType;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -17,7 +15,6 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ModInfo.MODID, name = ModInfo.NAME, version = ModInfo.VERSION, dependencies = ModInfo.DEPENDENCIES, acceptableRemoteVersions = "*")
 public class HardcoreIsland {
-
     @Instance
     public static HardcoreIsland instance;
 
@@ -25,30 +22,16 @@ public class HardcoreIsland {
     public static CommonProxy proxy;
 
     public HardcoreIsland() {
-        this.biomes = new HCIslandBiomes();
         this.loot = new HCIslandLootTables();
     }
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Content
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
     public static final WorldType WORLD_TYPE = new WorldTypeHCIsland();
 
-    private final HCIslandBiomes biomes;
     private final HCIslandLootTables loot;
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// FML Events
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @EventHandler
     public void onInit(FMLPreInitializationEvent event) {
         ConfigManager.sync(ModInfo.MODID, Config.Type.INSTANCE);
-        MinecraftForge.EVENT_BUS.register(biomes);
-        biomes.initBiomes();
     }
 
     @EventHandler
--- src/main/java/jakojaannos/hcisland/init/HCIslandBiomes.java
+++ src/main/java/jakojaannos/hcisland/init/HCIslandBiomes.java
@@ -2,6 +2,7 @@ package jakojaannos.hcisland.init;
 
 import jakojaannos.hcisland.ModInfo;
 import jakojaannos.hcisland.world.biome.*;
+import lombok.val;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
@@ -9,14 +10,13 @@ import net.minecraftforge.common.BiomeDictionary.Type;
 import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.common.BiomeManager.BiomeType;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.registries.IForgeRegistry;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @ObjectHolder(ModInfo.MODID)
+@Mod.EventBusSubscriber
 public class HCIslandBiomes {
     public static final Biome ISLAND = null;
     public static final Biome ISLAND_BEACH = null;
@@ -25,53 +25,22 @@ public class HCIslandBiomes {
     public static final Biome WASTELAND_BEACH = null;
     public static final Biome WASTELAND_EDGE = null;
 
-    public void initBiomes() {
-        // @formatter:off
-        register("island",          0, BiomeType.WARM,      new BiomeHCIsland(),         Type.FOREST, Type.SPARSE);
-        register("island_beach",    0, BiomeType.WARM,      new BiomeHCIslandBeach(),    Type.BEACH);
-        register("ocean",           0, BiomeType.WARM,      new BiomeHCOcean(),          Type.OCEAN, Type.DEAD);
-        register("wasteland",       0, BiomeType.DESERT,    new BiomeHCWasteland(),      Type.WASTELAND, Type.DEAD, Type.HOT);
-        register("wasteland_beach", 0, BiomeType.DESERT,    new BiomeHCWastelandBeach(), Type.WASTELAND, Type.BEACH);
-        register("wasteland_edge",  0, BiomeType.DESERT,    new BiomeHCWastelandEdge(),  Type.WASTELAND, Type.HOT);
-        // @formatter:on
-    }
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Private implementation
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    private final Map<String, Entry> biomes = new HashMap<>();
-
-    protected void register(String key, int weight, BiomeManager.BiomeType type, Biome biome, BiomeDictionary.Type... types) {
-        biomes.put(key, new Entry(biome, weight, type, types));
-    }
-
-
     @SubscribeEvent
-    public void onRegisterBiomes(RegistryEvent.Register<Biome> event) {
-        biomes.forEach((s, entry) -> doRegisterBiome(event.getRegistry(), s, entry));
+    public static void onRegisterBiomes(RegistryEvent.Register<Biome> event) {
+        val r = event.getRegistry();
+        register(r, "island", 0, BiomeType.WARM, new BiomeHCIsland(), Type.FOREST, Type.SPARSE);
+        register(r, "island_beach", 0, BiomeType.WARM, new BiomeHCIslandBeach(), Type.BEACH);
+        register(r, "ocean", 0, BiomeType.WARM, new BiomeHCOcean(), Type.OCEAN, Type.DEAD);
+        register(r, "wasteland", 0, BiomeType.DESERT, new BiomeHCWasteland(), Type.WASTELAND, Type.DEAD, Type.HOT);
+        register(r, "wasteland_beach", 0, BiomeType.DESERT, new BiomeHCWastelandBeach(), Type.WASTELAND, Type.BEACH);
+        register(r, "wasteland_edge", 0, BiomeType.DESERT, new BiomeHCWastelandEdge(), Type.WASTELAND, Type.HOT);
     }
 
-    private void doRegisterBiome(IForgeRegistry<Biome> registry, String key, Entry entry) {
-        entry.biome.setRegistryName(new ResourceLocation(ModInfo.MODID, key));
-        registry.register(entry.biome);
+    private static void register(IForgeRegistry<Biome> registry, String key, int weight, BiomeManager.BiomeType managerType, Biome biome, BiomeDictionary.Type... types) {
+        biome.setRegistryName(new ResourceLocation(ModInfo.MODID, key));
+        registry.register(biome);
 
-        BiomeManager.addBiome(entry.type, new BiomeManager.BiomeEntry(entry.biome, entry.weight));
-        BiomeDictionary.addTypes(entry.biome, entry.dictTypes);
-    }
-
-    private class Entry {
-        Biome biome;
-        int weight;
-        BiomeManager.BiomeType type;
-        BiomeDictionary.Type[] dictTypes;
-
-        private Entry(Biome biome, int weight, BiomeManager.BiomeType type, BiomeDictionary.Type... dictTypes) {
-            this.biome = biome;
-            this.weight = weight;
-            this.type = type;
-            this.dictTypes = dictTypes;
-        }
+        BiomeManager.addBiome(managerType, new BiomeManager.BiomeEntry(biome, weight));
+        BiomeDictionary.addTypes(biome, types);
     }
 }
